0166d1fbc16e3a468230853b2f8caf1eeafe2c09
tcs-r-nakamura <rnakamura.tcs@gmail.com>
2025-07-03 10:16:52 +0900

    first commit

---
+++ Problem1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program1
+{
+    static void Main(string[] args)
+    {
+        //2～100までループさせる
+        for (int number = 2; number <= 100; number++)
+        {//素数だったらture
+            bool answer = true;
+            //2～numberまでループ
+            for (int i = 2; i < number; i++)
+            {//割り切れたら素数じゃない
+                if (number % i == 0)
+                {
+                    answer = false;
+                    break;
+
+                }
+            }//もし素数だったら
+            if (answer)
+            {//素数を表示
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
