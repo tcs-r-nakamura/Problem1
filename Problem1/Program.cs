92a422703d2dc5215c61eb8401cd80aae209902f
tcs-r-nakamura <rnakamura.tcs@gmail.com>
2025-07-07 09:19:36 +0900

    メソッド分割

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -4,24 +4,37 @@ class Program1
 {
     static void Main(string[] args)
     {
-        //2～100までループさせる
-        for (int number = 2; number <= 100; number++)
-        {//素数だったらture
-            bool answer = true;
-            //2～numberまでループ
-            for (int i = 2; i < number; i++)
-            {//割り切れたら素数じゃない
-                if (number % i == 0)
-                {
-                    answer = false;
-                    break;
+        int startNumber = 1;
+        int endNumber = 100;
+        //1～100までループさせる
+        for (int number = startNumber; number <= endNumber; number++)
+        {
+            if (IsPrime(number))
+            {
 
-                }
-            }//もし素数だったら
-            if (answer)
-            {//素数を表示
                 Console.WriteLine(number);
             }
         }
+    }
+    /*素数判別メソッド　
+     * 戻り値に素数が入る*/
+    static bool IsPrime(int number)
+    {//もしnumberが1だったらfalseを返す
+        if (number == 1)
+        {
+            return false;
+        }
+        /*2～numberまでループ
+         もっと処理を軽く改良*/
+        for (int i = 2; i * i <= number; i++)
+        {
+            //割り切れたらfalseを返す
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }//もし割り切れなかったらtrueを返す
+        return true;
+
     }
 }
