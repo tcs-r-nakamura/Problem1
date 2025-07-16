0744704ffa212fe37e72b67b90f44b5de3212a43
tcs-r-nakamura <rnakamura.tcs@gmail.com>
2025-07-16 11:29:37 +0900

    素数判別メソッドの効率化

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -24,23 +24,20 @@ private static void showPrimeNumber(int startNumber, int endNumber)
 
     /*素数判別メソッド　
      * 戻り値に素数が入る*/
-    private static bool IsPrime(int number)
-{//もしnumberが1だったらfalseを返す
-    if (number == 1)
+    static bool IsPrime(int number)
     {
-        return false;
-    }
-    /*2～numberまでループ
-     もっと処理を軽く改良*/
-    for (int i = 2; i * i <= number; i ++)
-    {
-        //割り切れたらfalseを返す
-        if (number % i == 0)
-        {
-            return false;
-        }
-    }//もし割り切れなかったらtrueを返す
-    return true;
+        if (number <= 1) return false;  // 1以下は素数じゃない
+        if (number % 2 == 0) return false; // 2以外の偶数は素数じゃない
+        if (number == 2) return true;   // 2は素数
 
-}
+        for (int i = 3; i * i <= number; i += 2) // 奇数だけチェック
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
